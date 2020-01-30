7fecb4a7a2b328554c87917c38a2462e1221a94c
sunlee <sunlee128@gmail.com>
2020-01-30 12:31:46 +1100

    CommentBox Component Test added

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
+
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+}); //cleans up the DOM created during the test
+
+//FullDOM rendering example
+it('has text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+})
