59af3c062abcc0433fcc7b478f69699c04f25ff9
sunlee <sunlee128@gmail.com>
2020-02-21 11:46:23 +1100

    Revert "testing events"
    This reverts commit 20b157f6c3a2030780756ffee2b51893376728d0.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import CommentBox from 'components/CommentBox';
-import { mount } from 'enzyme';
+import React, { Component } from 'react'
+import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
 
 let wrapped;
 beforeEach(() => {
@@ -9,32 +9,10 @@ beforeEach(() => {
 
 afterEach(() => {
   wrapped.unmount();
-}); // cleans up the DOM created during the test
+}); //cleans up the DOM created during the test
 
-// FullDOM rendering example
+//FullDOM rendering example
 it('has text area and a button', () => {
-  expect(wrapped.find('textarea').length).toEqual(1);
-  expect(wrapped.find('button').length).toEqual(1);
-});
-
-describe('the text area', () => {
-  beforeEach(() => {
-    // simulating change event
-    wrapped.find('textarea').simulate('change', {
-      target: { value: 'new comment' } // fake event
-    });
-
-    // force it to rerender
-    wrapped.update();
-  });
-  it('has text area that users can type in', () => {
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-  });
-
-  it('when form is submitted, text area gets emptied', () => {
-    wrapped.find('form').simulate('submit');
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
-  });
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
 })
-;
